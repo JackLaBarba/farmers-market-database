131ea11878c3f47adfbccadce4bc7904da43356a
John (Jack) La Barba <labarbaj@oregonstate.edu>
2022-07-24 19:16:31 -0700

    add external stylesheets to react app

--- app-client/src/pages/SiteContainer.js
+++ app-client/src/pages/SiteContainer.js
@@ -6,6 +6,11 @@ import {
 export default function SiteContainer() {
   return (
     <div>
+      <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto" />
+      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" />
+      <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" />
+      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css" />
       <h2>Farmers Market Database</h2>
       <nav>
         <ul>
